6d73894c912333a276609942a0ccf9b2cd3f4996
you7588 <yo7588@gmail.com>
2017-08-11 16:52:12 +0800

    add container to applicant

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:create, :destroy]
+
   def index
     @posts = Post.order("id DESC").all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  protected
+
+  def post_params
+    patams.require(:post).permit(:content)
   end
 end
